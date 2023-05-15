58a51acc1e5d518849bf00771d364fd496ef279b
johnmillerdeguzman <johnmillerdeguzman@gmail.com>
2023-05-15 15:27:57 +0800

    Pushing Portfolio

--- app.js
+++ app.js
@@ -6,5 +6,24 @@ menu.addEventListener('click', function() {
   menuLinks.classList.toggle('active');
 });
 
+// // Get the modal
+// var modal = document.querySelector('.modal');
 
+// // Get the image and insert it inside the modal
+// var images = document.querySelectorAll('.tile img');
+// var modalImg = document.querySelector('.modal-content');
+// for (var i = 0; i < images.length; i++) {
+//   images[i].addEventListener('click', function() {
+//     modal.style.display = 'block';
+//     modalImg.src = this.src;
+//   });
+// }
+
+// // Get the <span> element that closes the modal
+// var closeBtn = document.querySelector('.close');
+
+// // When the user clicks on <span> (x), close the modal
+// closeBtn.addEventListener('click', function() {
+//   modal.style.display = 'none';
+// });
 
